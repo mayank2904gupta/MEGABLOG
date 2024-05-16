ec8a0f3934d01ed821719ddbde27e2b8641b53d3
mayank2904gupta <139375704+mayank2904gupta@users.noreply.github.com>
2024-05-16 15:45:21 +0530

    mega blog done after debugging

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Input from "./Input"
+import Header from "./header/Header";
+import Footer from "./Footer/Footer"
+import Container from "./container/Container";
+import Logo from './Logo'
+import LogoutBtn from "./header/LogoutBtn";
+import Button from './Button'
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login"
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard"
+import AuthLayout from "./AuthLayout"
+import Select from "./Select"
+
+export{
+    Select,
+    Input,
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Button,
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout
+}
